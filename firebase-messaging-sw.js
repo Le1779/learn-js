1c8a50fd89ea8b24b488ce8816bddb8627cbbc7f
DYNACOMWARE\kevinle <kevinle@dynacw.com>
2019-06-27 13:31:52 +0800

    add firebase notification

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -9,13 +9,13 @@ messaging = firebase.messaging();
 messaging.setBackgroundMessageHandler(function (payload) {
     console.log('[firebase-messaging-sw.js] Received background message ', payload);
     // Customize notification here
-    //var notificationTitle = 'Background Message Title';
-    //var notificationOptions = {
-    //  body: 'Background Message body.',
-    //  icon: 'firebase-logo.png'
-    //};
-    //
-    //return self.registration.showNotification(notificationTitle,
-    //notificationOptions);
+    var notificationTitle = 'Background Message Title';
+    var notificationOptions = {
+      body: 'Background Message body.',
+      icon: 'firebase-logo.png'
+    };
+    
+    return self.registration.showNotification(notificationTitle,
+    notificationOptions);
 });
 // [END background_handler]
