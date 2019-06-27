941c1fcae0a010cb5f242a93786afa8b3ca008e5
DYNACOMWARE\kevinle <kevinle@dynacw.com>
2019-06-27 13:24:02 +0800

    fix firebasee-messageing-sw.js 擺放位置錯誤

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,21 @@
+importScripts('https://www.gstatic.com/firebasejs/6.2.3/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/6.2.3/firebase-messaging.js');
+
+var firebaseConfig = {
+    messagingSenderId: "623688412760"
+};
+firebase.initializeApp(firebaseConfig);
+messaging = firebase.messaging();
+messaging.setBackgroundMessageHandler(function (payload) {
+    console.log('[firebase-messaging-sw.js] Received background message ', payload);
+    // Customize notification here
+    //var notificationTitle = 'Background Message Title';
+    //var notificationOptions = {
+    //  body: 'Background Message body.',
+    //  icon: 'firebase-logo.png'
+    //};
+    //
+    //return self.registration.showNotification(notificationTitle,
+    //notificationOptions);
+});
+// [END background_handler]
