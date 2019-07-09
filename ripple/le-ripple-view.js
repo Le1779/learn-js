d8d03b98a30c79137b333056d046ef7e55ddd217
DYNACOMWARE\kevinle <kevinle@dynacw.com>
2019-07-09 18:28:03 +0800

    simplify bezier, ripple view, phone view

--- ripple/le-ripple-view.js
+++ ripple/le-ripple-view.js
@@ -48,87 +48,11 @@ var RippleView = function (builder) {
         drawRipple(ripple2 %= 60);
         drawRipple(ripple3 %= 60);
         drawRipple(ripple4 %= 60);
-        //drawCenter();
-        //drawRipple(ripple5 %= 60);
 
         ripple1++;
         ripple2++;
         ripple3++;
         ripple4++;
-
-        //ripple5++;
-        //drawBackgroundCircle();
-        //drawHint();
-        //drawProgressCircle();
-        //drawPercent();
-        //drawProgressText();
-
-        function drawBackgroundCircle() {
-            context.save();
-            context.beginPath();
-            context.strokeStyle = "#E6E6E6";
-            context.lineWidth = viewWidth / 20;
-            context.arc(centerX, centerY + viewHeight / 4 - 15, viewWidth * 0.3, 0.1 * Math.PI, 0.9 * Math.PI, true);
-            context.stroke();
-            context.closePath();
-            context.restore();
-        }
-
-        function drawHint() {
-            context.save();
-            context.fillStyle = "#AAA";
-            context.font = "15px Arial";
-            context.textAlign = "start";
-            context.textBaseline = "middle";
-            context.fillText("0%", centerX - viewWidth * 0.3, viewHeight - 15);
-            context.textAlign = "end";
-            context.fillText("100%", centerX + viewWidth * 0.3, viewHeight - 15);
-            context.restore();
-        }
-
-        function drawProgressCircle() {
-            context.save();
-            let percentagePoint = 1.2 / max;
-            let percent = -1.1 + progress * percentagePoint;
-            context.beginPath();
-            context.strokeStyle = "#00C58D";
-            context.lineWidth = viewWidth / 20;
-            context.arc(centerX, centerY + viewHeight / 4 - 15, viewWidth * 0.3, percent * Math.PI, 0.9 * Math.PI, true);
-            context.stroke();
-            context.closePath();
-            context.restore();
-        }
-
-        function drawPercent() {
-            context.save();
-            let percent = Math.round(progress / max * 1000) / 10;
-            context.fillStyle = "#3D4060";
-            context.font = "bold 60px Arial";
-            context.textAlign = "center";
-            context.textBaseline = "middle";
-            context.fillText(percent + "%", centerX, viewHeight - 60);
-            context.restore();
-        }
-
-        function drawProgressText() {
-            context.save();
-            let percent = Math.round(progress / max * 1000) / 10;
-            context.font = "30px Arial";
-            context.textBaseline = "middle";
-
-            context.fillStyle = "#00C58D";
-            context.textAlign = "end";
-            context.fillText(progress, centerX - 10, centerY);
-
-            context.fillStyle = "#3D4060";
-            context.textAlign = "center";
-            context.fillText("/", centerX, centerY);
-
-            context.fillStyle = "#AAA";
-            context.textAlign = "start";
-            context.fillText(max, centerX + 10, centerY);
-            context.restore();
-        }
     }
 
     function drawCenter() {
@@ -150,10 +74,6 @@ var RippleView = function (builder) {
         let radius = viewWidth * 0.48 * (frame / 60);
 
         let width = viewWidth * 0.4 * 0.3;
-        //if(radius <= width){            
-        //    width *= (frame / 0.3 / 60);
-        //    radius = width;
-        //}
         context.globalAlpha = 1 - frame / 60;
         context.lineWidth = width;
         context.arc(centerX, -viewWidth * 0.060, radius, 0, 2 * Math.PI);
