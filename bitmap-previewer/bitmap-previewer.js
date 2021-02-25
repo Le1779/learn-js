6c345932590f9a13d8707446ae789b32d6de0b9c
kevinle <le8419951779@gmail.com>
2021-02-25 09:37:35 +0800

    [Bitmap Previewer] fix bug

--- bitmap-previewer/bitmap-previewer.js
+++ bitmap-previewer/bitmap-previewer.js
@@ -26,8 +26,8 @@ var BitmapPreviewer = function (builder) {
 
             input = $('#bitmap-preview-input');
             input.keyup(function () {
-                onScreenResize();
                 generateFontCodeList();
+                onScreenResize();
             });
         }
 
