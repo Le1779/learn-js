97d388d658f0adaf279289b72d5e1f26d51577d8
kevinle <le8419951779@gmail.com>
2021-05-05 10:20:23 +0800

    [LockTooltip][Update] 移除builder的方法

--- working-order/lock-tooltip/lock-tooltip.js
+++ working-order/lock-tooltip/lock-tooltip.js
@@ -1,40 +1,73 @@
 class LockTooltip {
-    constructor(builder) {
-        this.builder = builder;
-        this.container = null;
-        this.button = null;
-        this.title = null;
-        this.subtitle = null;
-        this.loadingText = null;
+    constructor(containerID, isPM, locker, visitor, require, action) {
+        this.containerID = containerID;
+        this.isPM = isPM;
+        this.locker = locker;
+        this.visitor = visitor;
+        this.require = require;
+        this.action = action;
+
+        this.title = "";
+        this.subtitle = "";
+        this.buttonText = "";
+        this.loadingText = "";
+
+        this._generateContainer();
+        this._generateExpandButton();
+        this._generateTooltip();
+        this._updateTooltipText();
     }
 
-    generateContainer(containerID) {
-        this.container = $(`#${containerID}`);
+    setPM(isPM) {
+        this.isPM = isPM;
+        this._updateTooltipText();
+    }
+
+    setLocker(locker) {
+        this.locker = locker;
+        this._updateTooltipText();
+    }
+
+    setVisitor(visitor) {
+        this.visitor = visitor;
+        this._updateTooltipText();
+    }
+
+    setRequire(require) {
+        this.require = require;
+        this._updateTooltipText();
+    }
+
+    setAction(action) {
+        this.action = action;
+    }
+
+    _generateContainer() {
+        this.container = $(`#${this.containerID}`);
         this.container.html("");
         this.container.css({ position: "relative", display: "inline-block"});
     }
 
-    generateButton(isLocked) {
+    _generateExpandButton() {
         if (!this.container) {
             return;
         }
 
-        this.container.append(`<div id="lock-tooltip-button" class="material-icons-outlined">${isLocked ? 'lock' : 'lock_open'}</div>`)
-        this.button = $('#lock-tooltip-button');
+        this.container.append(`<div id="lock-tooltip-button" class="material-icons-outlined">${this.locker ? 'lock' : 'lock_open'}</div>`)
+        this.expandButton = $('#lock-tooltip-button');
 
-        this.button.click(function(e) {
+        this.expandButton.click(function(e) {
             e.stopPropagation();
             $('#lock-tooltip').toggleClass('active');
         })
     }
 
-    generateTooltip(title, subtitle, buttonText) {
-        var buttonHtml = buttonText == "" ? "" : `<div id="lock-tooltip-action">${buttonText}</div>`;
+    _generateTooltip() {
         this.container.append(
             `<div id="lock-tooltip" class="active">
-                <div id="lock-tooltip-title">${title}</div>
-                <div id="lock-tooltip-subtitle">${subtitle}</div>
-                ${buttonHtml}
+                <div id="lock-tooltip-title"></div>
+                <div id="lock-tooltip-subtitle"></div>
+                <div id="lock-tooltip-action"></div>
                 <div id="lock-tooltip-load-container">
                     <div id="lock-tooltip-load-text">${"處理中..."}</div>
                     <div id="lock-tooltip-load"></div>
@@ -44,12 +77,12 @@ class LockTooltip {
 
         this.title = $('#lock-tooltip-title');
         this.subtitle = $('#lock-tooltip-subtitle');
-        this.loadingText = $('#lock-tooltip-load-text');
+        this.tooltipButton = $('#lock-tooltip-action');
 
         var self = this;
-        $('#lock-tooltip-action').click(function() {
+        $('#lock-tooltip-action').click(function(event) {
             event.stopPropagation();
-            console.log(self.builder.isLocked)
+            self.action();
         })
 
         $('#lock-tooltip').click(function(event) {
@@ -60,64 +93,40 @@ class LockTooltip {
             $('#lock-tooltip').removeClass('active');
         })
     }   
-}
 
-class LockTooltipBuilder {
-    constructor(containerID) {
-        this.containerID = containerID;
-
-        this.isLocked = false;
-        this.isPM = false;
-        this.locker = null;
-        this.visitor = null;
-        this.host = null;
-
-        this.title = "";
-        this.subtitle = "";
-        this.buttonText = "";
-        this.loadingText = "";
-    }
-
-    setLocked(isLocked) {
-        this.isLocked = isLocked;
-        return this;
-    }
-
-    setPM(isPM) {
-        this.isPM = isPM;
-        return this;
-    }
-
-    setLocker(locker) {
-        this.locker = locker;
-        return this;
-    }
-
-    setVisitor(visitor) {
-        this.visitor = visitor;
-        return this;
-    }
-
-    setHost(host) {
-        this.host = host;
-        return this;
-    }
-
-    build() {
-        var instance = new LockTooltip(this);
-        instance.generateContainer(this.containerID);
-        instance.generateButton(this.isLocked);
-
-        if (this.isPM || this.locker.id != this.visitor.id) {
-            this.title = this.isLocked ? "工單已被鎖定" : "工單未鎖定";
-            this.subtitle = this.isLocked ? `這是由${this.locker.name}提出的鎖定` : "";
+    _updateTooltipText() {
+        if (this.locker) {
+            this.title.html("工單已被鎖定");
+            if (this._isLocker()) {
+                if (this.require == null) {
+                    this.subtitle.html("你已經鎖定此工單");
+                } else {
+                    this.subtitle.html(`${this.require.name} 向你提出解鎖的請求`);
+                }
+                
+                this.tooltipButton.html("解鎖");
+            } else {
+                this.subtitle.html(`此工單已由 ${this.locker.name} 鎖定`);
+                if (this.isPM) {
+                    this.tooltipButton.html("請求解鎖");
+                } else {
+                    this.tooltipButton.html("確認");
+                }
+            }
         } else {
-            this.title = this.isLocked ? "工單已被鎖定" : "鎖定工單";
-            this.subtitle = this.isLocked ? `解除鎖定讓PM可以更新工單檔案` : "此動作會造成PM無法更新工單檔案";
-            this.buttonText = this.isLocked ? "解鎖" : "鎖定"
-        }
+            this.title.html("工單未鎖定");
 
-        instance.generateTooltip(this.title, this.subtitle, this.buttonText)
-        return instance;
+            if (this.isPM) {
+                this.subtitle.html("如果其他人鎖定此工單，你將無法編輯工單");
+                this.tooltipButton.html("確認");
+            } else {
+                this.subtitle.html("此動作會造成PM無法更新工單檔案");
+                this.tooltipButton.html("鎖定");
+            }
+        }
+    }
+
+    _isLocker() {
+        return this.locker.id == this.visitor.id
     }
 }
